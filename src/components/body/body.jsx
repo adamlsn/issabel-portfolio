0e267c9e56122c69d635431fb0108c83ef0bf22b
Adam Olson <adamlsn@gmail.com>
2022-01-07 18:32:40 -0600

    fleshing out body section:

--- src/components/body/body.jsx
+++ src/components/body/body.jsx
@@ -5,14 +5,15 @@ const Body = () => {
     return (
         <div className="body">
             <div className="body-left">
-                <div className="body-card background"></div>
                 <div className="body-card">
                     <img src={Image} alt="" className="body-img" />
                 </div>
             </div>
 
             <div className="body-right">
-
+                <h1 className="body-title">About Me</h1>
+                <p className="body-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <p className="body-description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus perspiciatis quasi, delectus quam nulla doloremque illo numquam, labore nobis pariatur modi aliquam at ullam ipsa, voluptate amet deleniti libero praesentium!</p>
             </div>
         </div>
     )
