09aee5af88bd24f4d272afaaa22a25b364448dc3
Adam Olson <adamlsn@gmail.com>
2022-01-16 18:28:08 -0600

    adding download for resume

--- src/components/body/body.jsx
+++ src/components/body/body.jsx
@@ -1,5 +1,7 @@
 import "./body.css"
 import Image from "../../img/body.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faFileAlt } from "@fortawesome/free-solid-svg-icons"
 
 const Body = () => {
     return (
@@ -17,7 +19,19 @@ const Body = () => {
                 
                 <p className="body-description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus perspiciatis quasi, delectus quam nulla doloremque illo numquam, labore nobis pariatur modi aliquam at ullam ipsa, voluptate amet deleniti libero praesentium!</p>
 
-                
+                <div className="body-resume">
+                    <a href="www.google.com" className="body-download-svg">
+                    <FontAwesomeIcon icon={faFileAlt} className="resume-icon"></FontAwesomeIcon>
+                    </a>
+                    <div className="body-resume-text">
+                        <h4 className="resume-title">
+                            If you'd like to see more
+                        </h4>
+                        <p className="resume-description">
+                            you can download a copy of my resume <a href="www.google.com" className="link">here</a>.
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     )
