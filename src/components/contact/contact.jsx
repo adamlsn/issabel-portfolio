aa1bb9fd81c914ace66f2994eecd187e22730d83
Adam Olson <adamlsn@gmail.com>
2022-01-25 10:11:24 -0600

    adding in EmailJS, testing functionality

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -3,8 +3,25 @@ import { faShoppingBag } from "@fortawesome/free-solid-svg-icons"
 import { faLinkedin, faInstagram } from "@fortawesome/free-brands-svg-icons"
 
 import "./contact.css"
+import { useRef, useState } from "react"
+import emailjs from "emailjs-com"
 
 const Contact = () => {
+    const form = useRef()
+    const [done, setDone] = useState(false)
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+    
+        emailjs.sendForm('service_z5xzsdb', 'template_7gc0x5e', form.current, 'user_2gqCBP9BBH7tPTG5pxYUL')
+          .then((result) => {
+              console.log(result.text);
+              setDone(true)
+          }, (error) => {
+              console.log(error.text);
+          });
+    }
+
     return (
         <div className="contact">
             <div className="contact-background"></div>
@@ -12,20 +29,20 @@ const Contact = () => {
                 <div className="contact-left">
                     <h1 className="contact-title">
                         My Links
-                    </h1>
+                    </h1>{}
                     <div className="contact-info">
                         <div>
-                            <a href="https://www.linkedin.com/in/issabel-schultz-1b3068137/"  className="contact-info-item">
+                            <a href="https://www.linkedin.com/in/issabel-schultz-1b3068137/" target="_blank"  rel="noreferrer noopener" className="contact-info-item">
                             <FontAwesomeIcon icon={faLinkedin} className="contact-icon"></FontAwesomeIcon>
                             LinkedIn</a>
                         </div>
                         <div>
-                            <a href="https://www.depop.com/issabelschultz/"  className="contact-info-item">
+                            <a href="https://www.depop.com/issabelschultz/" target="_blank" rel="noreferrer noopener" className="contact-info-item">
                             <FontAwesomeIcon icon={faShoppingBag} className="contact-icon"></FontAwesomeIcon>
                             Depop</a>
                         </div>
                         <div>
-                            <a href="https://www.instagram.com/issyschultz/?hl=en" className="contact-info-item">
+                            <a href="https://www.instagram.com/issyschultz/?hl=en" target="_blank" rel="noreferrer noopener" className="contact-info-item">
                             <FontAwesomeIcon icon={faInstagram} className="contact-icon"></FontAwesomeIcon>
                             Instagram</a>
                         </div>
@@ -35,12 +52,14 @@ const Contact = () => {
                     <p className="contact-description">
                         <b>Let me know</b> if you'd want to speak with me, or if you have any general questions by filling out the form below.
                     </p>
-                    <form>
+                    <form ref={form} onSubmit={handleSubmit}>
                         <input type="text" placeholder="Name" name="contact_name"/>
                         <input type="text" placeholder="Subject" name="contact_subject"/>
                         <input type="text" placeholder="Email" name="contact_email"/>
                         <textarea rows="5" placeholder="Message" name="contact_message"></textarea>
                         <button>Submit</button>
+
+                        {done && " Email sent, thank you!"}
                     </form>
                 </div>
             </div>
